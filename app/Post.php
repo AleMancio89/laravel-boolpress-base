af236b8e80cf43b8fc24a8d609d8d20982130ea0
Alessandro <alessandro.mancinelli89@gmail.com>
2021-02-03 17:18:26 +0100

    Enabled DatabaseSeeder and tabs relations

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    public function postInformation() {
+
+        return $this->hasOne('App\PostInformation', 'post_id', 'id');
+
+    }
+
+    public function category() {
+
+        return $this->belongsTo('App\Category');
+
+    }
 }
