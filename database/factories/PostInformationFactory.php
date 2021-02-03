ee345ea3d8840aba4924d60e99361b8e6b040456
Alessandro <alessandro.mancinelli89@gmail.com>
2021-02-03 11:24:40 +0100

    minor fixes

--- database/factories/PostInformationFactory.php
+++ database/factories/PostInformationFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(PostInformation::class, function (Faker $faker) {
     return [
-        'post_id' => $faker->numberBetween($min = 1, $max = 100),
+        'post_id' => $faker->unique()->numberBetween($min = 1, $max = 100),
         'description' => $faker->paragraph(),
         'slug' => $faker->slug(),
         'created_at' => $faker->date(),
